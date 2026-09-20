59296067b18c6ed7929abe4e387f271ff61ab812
hsupriyatno <hsupriyatno@yahoo.com>
2026-09-20 08:53:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,8 @@ selected = option_menu(
 
 if selected == "Home":
     st.markdown("<h1 style='text-align: center; margin-bottom: 20px;'>☕ Welcome to Sempat Kuliner</h1>", unsafe_allow_html=True)
-    if os.path.exists("foto_cafe.jpg"):
-        st.image("foto_cafe.jpg", use_container_width=True)
+    if os.path.exists("foto_cafe.jpeg"):
+        st.image("foto_cafe.jpeg", use_container_width=True)
     else:
         st.image("https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?q=80&w=800", use_container_width=True)
     st.markdown("<h3 style='text-align: center; color: #6D4C41;'>Where Every Sip Tells a Story</h3>", unsafe_allow_html=True)
